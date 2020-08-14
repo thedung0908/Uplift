cf93915c94c6c1a02d2fae1ca34d9b23787a5294
thedung0908 <thedung0908@gmail.com>
2020-08-14 19:08:47 +0700

    Add code for Repository pattern

--- Uplift.DataAccess/Data/ApplicationDbContext.cs
+++ Uplift.DataAccess/Data/ApplicationDbContext.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Uplift.Models;
 
 namespace Uplift.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Category> Category { get; set; }
     }
 }
